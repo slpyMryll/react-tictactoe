d83176e4c11a6fc5625c2ca4d937487e4a1da4fc
Marylle Laguna <maryllelagunaa@gmail.com>
2025-09-19 23:01:03 +0800

    Update font style

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 type SquareProps = {
   value: string | null;
   onClick: () => void;
@@ -10,7 +9,7 @@ export default function Square({ value, onClick }: SquareProps) {
   return (
     <button
       onClick={onClick}
-      className="w-20 h-20 border-2 border-black text-3xl font-bold flex items-center justify-center bg-[#D9D9D9] rounded-3xl"
+      className="w-20 h-20 border-2 border-black text-3xl font-[Acme] flex items-center justify-center bg-[#D9D9D9] rounded-3xl text-[#363062]"
     >
       {value}
     </button>
